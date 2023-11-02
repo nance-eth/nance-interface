a56734a7996f67ad3526d8a92b26eee2181de429
twodam <twodam@protonmail.com>
2023-11-02 19:12:18 +0800

    refactor: move components from pages/landing to Landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Footer, SiteNav } from "@/components/Site";
-import Landing from "../components/pages/landing/Landing";
+import Landing from "@/components/Landing";
 
 export default function LandingPage() {
   return (
