7d0224d1c07556e7d11deaf1edb24e6f4f7f607c
twodam <twodam@protonmail.com>
2023-11-16 22:00:17 +0800

    feat: add function isValidSafe which used plain fetch

--- utils/hooks/SafeHooks.ts
+++ utils/hooks/SafeHooks.ts
@@ -1,12 +1,26 @@
-import useSWR, { Fetcher } from 'swr';
-import { RevisedSafeMultisigTransactionResponse, SafeBalanceUsdResponse, SafeDelegatesResponse, SafeInfoResponse } from '../../models/SafeTypes';
-import { useCallback, useEffect, useState } from 'react';
-import { useAccount, useWalletClient } from 'wagmi';
-import SafeApiKit, { SafeMultisigTransactionListResponse } from '@safe-global/api-kit';
-import { useEthersSigner } from './ViemAdapter';
-import Safe, { EthersAdapter, SafeTransactionOptionalProps } from '@safe-global/protocol-kit';
-import { ethers } from 'ethers';
-import { MetaTransactionData, SafeTransaction, SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types';
+import useSWR, { Fetcher } from "swr";
+import {
+  RevisedSafeMultisigTransactionResponse,
+  SafeBalanceUsdResponse,
+  SafeDelegatesResponse,
+  SafeInfoResponse,
+} from "../../models/SafeTypes";
+import { useCallback, useEffect, useState } from "react";
+import { useAccount, useWalletClient } from "wagmi";
+import SafeApiKit, {
+  SafeMultisigTransactionListResponse,
+} from "@safe-global/api-kit";
+import { useEthersSigner } from "./ViemAdapter";
+import Safe, {
+  EthersAdapter,
+  SafeTransactionOptionalProps,
+} from "@safe-global/protocol-kit";
+import { ethers } from "ethers";
+import {
+  MetaTransactionData,
+  SafeTransaction,
+  SafeTransactionDataPartial,
+} from "@safe-global/safe-core-sdk-types";
 
 const SAFE_SERVICE_API = "https://safe-transaction-mainnet.safe.global";
 const SAFE_API_V1_ROOT = SAFE_SERVICE_API + "/api/v1/";
@@ -16,11 +30,11 @@ function jsonFetcher(): Fetcher<SafeMultisigTransactionListResponse, string> {
   return async (url) => {
     const res = await fetch(url);
     if (res.status == 400) {
-      throw new Error('Invalid data.');
+      throw new Error("Invalid data.");
     } else if (res.status == 422) {
-      throw new Error('Invalid ethereum address.');
+      throw new Error("Invalid ethereum address.");
     } else if (res.status == 404) {
-      throw new Error('Safe not found.');
+      throw new Error("Safe not found.");
     }
     const json = await res.json();
 
@@ -28,37 +42,65 @@ function jsonFetcher(): Fetcher<SafeMultisigTransactionListResponse, string> {
   };
 }
 
-export function useMultisigTransactionOf(address: string, safeTxHash: string, shouldFetch: boolean = true) {
+export function useMultisigTransactionOf(
+  address: string,
+  safeTxHash: string,
+  shouldFetch: boolean = true,
+) {
   return useSWR(
-    shouldFetch ? `${SAFE_API}${address}/multisig-transactions/?safe_tx_hash=${safeTxHash}` : null,
+    shouldFetch
+      ? `${SAFE_API}${address}/multisig-transactions/?safe_tx_hash=${safeTxHash}`
+      : null,
     jsonFetcher(),
   );
 }
 
-export function useHistoryTransactions(address: string, limit: number = 10, shouldFetch: boolean = true) {
+export function useHistoryTransactions(
+  address: string,
+  limit: number = 10,
+  shouldFetch: boolean = true,
+) {
   return useSWR(
-    shouldFetch ? `${SAFE_API}${address}/multisig-transactions/?executed=true&trusted=true&limit=${limit}` : null,
+    shouldFetch
+      ? `${SAFE_API}${address}/multisig-transactions/?executed=true&trusted=true&limit=${limit}`
+      : null,
     jsonFetcher(),
   );
 }
 
-export function useQueuedTransactions(address: string, nonceGte: number, limit: number = 10, shouldFetch: boolean = true) {
+export function useQueuedTransactions(
+  address: string,
+  nonceGte: number,
+  limit: number = 10,
+  shouldFetch: boolean = true,
+) {
   return useSWR(
-    shouldFetch ? `${SAFE_API}${address}/multisig-transactions/?nonce__gte=${nonceGte}&trusted=true&limit=${limit}` : null,
+    shouldFetch
+      ? `${SAFE_API}${address}/multisig-transactions/?nonce__gte=${nonceGte}&trusted=true&limit=${limit}`
+      : null,
     jsonFetcher(),
   );
 }
 
-export function useMultisigTransactions(address: string, limit: number = 10, shouldFetch: boolean = true) {
+export function useMultisigTransactions(
+  address: string,
+  limit: number = 10,
+  shouldFetch: boolean = true,
+) {
   return useSWR(
-    shouldFetch ? `${SAFE_API}${address}/multisig-transactions/?trusted=true&limit=${limit}` : null,
+    shouldFetch
+      ? `${SAFE_API}${address}/multisig-transactions/?trusted=true&limit=${limit}`
+      : null,
     jsonFetcher(),
   );
 }
 
 // react hook wrapper for safe api kit
 // FIXME this will trigger infinite re-rendering
-export function useSafeAPIFunction<T>(functionWrapper: (safeApiKit: SafeApiKit) => Promise<T>, shouldFetch: boolean = true) {
+export function useSafeAPIFunction<T>(
+  functionWrapper: (safeApiKit: SafeApiKit) => Promise<T>,
+  shouldFetch: boolean = true,
+) {
   const [error, setError] = useState<string>();
   const [loading, setLoading] = useState(false);
   const [value, setValue] = useState<T>();
@@ -66,8 +108,9 @@ export function useSafeAPIFunction<T>(functionWrapper: (safeApiKit: SafeApiKit) 
   const { value: safeApiKit } = useSafeAPIKit();
 
   const _functionWrapper = useCallback(
-    (safeApiKit: SafeApiKit) => functionWrapper(safeApiKit), [functionWrapper]
-  )
+    (safeApiKit: SafeApiKit) => functionWrapper(safeApiKit),
+    [functionWrapper],
+  );
 
   const refetch = async () => {
     if (!safeApiKit || !shouldFetch) {
@@ -87,21 +130,24 @@ export function useSafeAPIFunction<T>(functionWrapper: (safeApiKit: SafeApiKit) 
   };
 
   useEffect(() => {
-    refetch()
-  }, [safeApiKit, shouldFetch, functionWrapper])
+    refetch();
+  }, [safeApiKit, shouldFetch, functionWrapper]);
 
   return {
-    value, error, loading, refetch
-  }
+    value,
+    error,
+    loading,
+    refetch,
+  };
 }
 
 function balanceJsonFetcher(): Fetcher<SafeBalanceUsdResponse[], string> {
   return async (url) => {
     const res = await fetch(url);
     if (res.status == 404) {
-      throw new Error('Safe not found.');
+      throw new Error("Safe not found.");
     } else if (res.status == 422) {
-      throw new Error('Safe address checksum not valid.');
+      throw new Error("Safe address checksum not valid.");
     }
     const json = await res.json();
 
@@ -109,9 +155,14 @@ function balanceJsonFetcher(): Fetcher<SafeBalanceUsdResponse[], string> {
   };
 }
 
-export function useMultisigAssets(address: string, shouldFetch: boolean = true) {
+export function useMultisigAssets(
+  address: string,
+  shouldFetch: boolean = true,
+) {
   return useSWR(
-    shouldFetch ? `${SAFE_API}${address}/balances/usd/?trusted=true&exclude_spam=true` : null,
+    shouldFetch
+      ? `${SAFE_API}${address}/balances/usd/?trusted=true&exclude_spam=true`
+      : null,
     balanceJsonFetcher(),
   );
 }
@@ -120,9 +171,9 @@ function safeInfoJsonFetcher(): Fetcher<SafeInfoResponse, string> {
   return async (url) => {
     const res = await fetch(url);
     if (res.status == 404) {
-      throw new Error('Safe not found.');
+      throw new Error("Safe not found.");
     } else if (res.status == 422) {
-      throw new Error('Safe address checksum not valid.');
+      throw new Error("Safe address checksum not valid.");
     }
     const json = await res.json();
 
@@ -134,7 +185,7 @@ export function useSafeInfo(address: string, shouldFetch: boolean = true) {
   return useSWR<SafeInfoResponse, Error>(
     shouldFetch ? `${SAFE_API}${address}` : null,
     safeInfoJsonFetcher(),
-    { shouldRetryOnError: false }
+    { shouldRetryOnError: false },
   );
 }
 
@@ -168,11 +219,11 @@ export function useSafeAPIKit() {
 
     const ethAdapter = new EthersAdapter({
       ethers,
-      signerOrProvider: signer!
+      signerOrProvider: signer!,
     });
     const safeApiKit = new SafeApiKit({
       txServiceUrl: SAFE_SERVICE_API,
-      ethAdapter
+      ethAdapter,
     });
     setValue(safeApiKit);
   }, [signer]);
@@ -192,19 +243,23 @@ export function useSafe(safeAddress: string) {
 
     const ethAdapter = new EthersAdapter({
       ethers,
-      signerOrProvider: signer
+      signerOrProvider: signer,
     });
     Safe.create({
       ethAdapter,
-      safeAddress
-    }).then(safe => setValue(safe))
-      .catch(err => setError(err));
+      safeAddress,
+    })
+      .then((safe) => setValue(safe))
+      .catch((err) => setError(err));
   }, [signer, safeAddress]);
 
   return { value, loading: !value, error };
 }
 
-export function useCreateTransaction(safeAddress: string, safeTransactionData: SafeTransactionDataPartial | MetaTransactionData[]) {
+export function useCreateTransaction(
+  safeAddress: string,
+  safeTransactionData: SafeTransactionDataPartial | MetaTransactionData[],
+) {
   const [error, setError] = useState<string>();
   const [loading, setLoading] = useState(false);
   const [value, setValue] = useState<SafeTransaction>();
@@ -219,20 +274,28 @@ export function useCreateTransaction(safeAddress: string, safeTransactionData: S
 
     setLoading(true);
 
-    safe.createTransaction({ safeTransactionData, onlyCalls: true })
-      .then(safeTransaction => setValue(safeTransaction))
-      .catch(err => setError(err)).finally(() => setLoading(false));
+    safe
+      .createTransaction({ safeTransactionData, onlyCalls: true })
+      .then((safeTransaction) => setValue(safeTransaction))
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
   }, [safe, safeTransactionData]);
 
   return {
-    value, error, loading
-  }
+    value,
+    error,
+    loading,
+  };
 }
 
-export function useQueueTransaction(safeAddress: string, safeTransactionData: SafeTransactionDataPartial | MetaTransactionData[], nonce?: number) {
+export function useQueueTransaction(
+  safeAddress: string,
+  safeTransactionData: SafeTransactionDataPartial | MetaTransactionData[],
+  nonce?: number,
+) {
   const [error, setError] = useState<string>();
   const [loading, setLoading] = useState(false);
-  const [value, setValue] = useState<{ safeTxHash: string, nonce: string }>();
+  const [value, setValue] = useState<{ safeTxHash: string; nonce: string }>();
 
   const { data: walletClient } = useWalletClient();
   const { address, isConnecting, isDisconnected } = useAccount();
@@ -246,13 +309,17 @@ export function useQueueTransaction(safeAddress: string, safeTransactionData: Sa
     }
 
     const options: SafeTransactionOptionalProps = {
-      nonce // Optional
-    }
+      nonce, // Optional
+    };
 
     setLoading(true);
 
     try {
-      const safeTransaction = await safe.createTransaction({ safeTransactionData, options, onlyCalls: true });
+      const safeTransaction = await safe.createTransaction({
+        safeTransactionData,
+        options,
+        onlyCalls: true,
+      });
       const senderAddress = address;
       const safeTxHash = await safe.getTransactionHash(safeTransaction);
       const signature = await safe.signTransactionHash(safeTxHash);
@@ -263,12 +330,12 @@ export function useQueueTransaction(safeAddress: string, safeTransactionData: Sa
         safeTransactionData: safeTransaction.data,
         safeTxHash,
         senderAddress,
-        senderSignature: signature.data
+        senderSignature: signature.data,
       });
 
       setValue({
         safeTxHash: safeTxHash,
-        nonce: safeTransaction.data.nonce.toString()
+        nonce: safeTransaction.data.nonce.toString(),
       });
     } catch (e: any) {
       setError(e.message);
@@ -278,24 +345,36 @@ export function useQueueTransaction(safeAddress: string, safeTransactionData: Sa
   };
 
   return {
-    value, error, loading, trigger
+    value,
+    error,
+    loading,
+    trigger,
   };
+}
+
+async function fetchSafeWithAddress(url: string) {
+  const res = await fetch(url);
+  if (res.status !== 200) {
+    return false;
+  }
+  const json = await res.json();
+  return json.address !== undefined;
 }
 
 function validSafeFetcher(): Fetcher<any, string> {
-  return async (url) => {
-    const res = await fetch(url);
-    if (res.status !== 200) {
-      return false;
-    }
-    const json = await res.json();
-    return json.address !== undefined;
-  };
+  return fetchSafeWithAddress;
 }
 
-export function useIsValidAddress(address: string, shouldFetch: boolean = true) {
+export function useIsValidAddress(
+  address: string,
+  shouldFetch: boolean = true,
+) {
   return useSWR(
     shouldFetch ? `${SAFE_API}${address}` : null,
     validSafeFetcher(),
   );
+}
+
+export async function isValidSafe(address: string) {
+  return fetchSafeWithAddress(`${SAFE_API}${address}`);
 }
