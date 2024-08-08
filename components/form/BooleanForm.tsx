bc4b4e8f35d6d83e2f5f4d5d1c4ecb8ff3224abe
twodam <twodam@protonmail.com>
2024-08-08 14:38:41 +0800

    feat(BooleanForm): add tooltip parameter

--- components/form/BooleanForm.tsx
+++ components/form/BooleanForm.tsx
@@ -1,27 +1,43 @@
 import { ErrorMessage } from "@hookform/error-message";
 import { useFormContext } from "react-hook-form";
+import TooltipInfo from "./TooltipInfo";
 
-export default function BooleanForm(
-  { label, fieldName, checked = false, showType = true } : { label: string, fieldName: any, checked?: boolean, showType?: boolean }
-) {
-  const { register, formState: { errors } } = useFormContext();
+export default function BooleanForm({
+  label,
+  fieldName,
+  checked = false,
+  showType = true,
+  tooltip = "",
+}: {
+  label: string;
+  fieldName: any;
+  checked?: boolean;
+  showType?: boolean;
+  tooltip?: string;
+}) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
 
   return (
     <div>
-      <label className="block text-sm font-medium text-gray-700">
-        {label}
-      </label>
+      <div className="text-sm font-medium text-gray-700 flex space-x-1 items-center">
+        <span>{label}</span> {tooltip && <TooltipInfo content={tooltip} />}
+      </div>
       <div className="mt-1 flex rounded-md shadow-sm">
-        { showType && (
+        {showType && (
           <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-                        bool
+            bool
           </span>
         )}
         <input
           type="checkbox"
           defaultChecked={checked}
           {...register(fieldName, { shouldUnregister: true })}
-          className={`block h-10 w-10 flex-1 rounded-r-md border-gray-300 ${showType ? 'rounded-r-none' : 'rounded-l-md'}`}
+          className={`block h-10 w-10 flex-1 rounded-r-md border-gray-300 ${
+            showType ? "" : "rounded-l-md"
+          }`}
         />
       </div>
       <ErrorMessage
