c0995c0996fa28f7508a3f25857a5c7ba0c02670
twodam <twodam@protonmail.com>
2024-08-21 15:51:06 +0800

    fix(SafekitWrapper): either we use correct txServiceUrl with api suffix, or we just specify the chainId and let it fetch

--- utils/hooks/Safe/SafekitWrapper.ts
+++ utils/hooks/Safe/SafekitWrapper.ts
@@ -1,19 +1,16 @@
 import { useContext, useEffect, useState } from "react";
 import SafeApiKit from "@safe-global/api-kit";
-import { useSafeNetworkAPI } from "@/utils/hooks/Safe/SafeURL";
 import { NetworkContext } from "@/context/NetworkContext";
 import { getChainByNetworkName } from "config/custom-chains";
 
 export function useSafeAPIKit() {
   const [value, setValue] = useState<SafeApiKit>();
   const network = useContext(NetworkContext);
-  const txServiceUrl = useSafeNetworkAPI();
 
   useEffect(() => {
     const chain = getChainByNetworkName(network);
     const safeApiKit = new SafeApiKit({
       chainId: BigInt(chain.id),
-      txServiceUrl,
     });
     setValue(safeApiKit);
   }, [network]);
