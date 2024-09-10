9741d3ff348cce2cbef37b0e512db66191a7a68f
twodam <twodam@protonmail.com>
2024-09-10 15:54:13 +0800

    feat(TenderlySimulationButton): parse Safe contract errCode if possible and log to console

--- components/TenderlySimulation/TenderlySimulationButton.tsx
+++ components/TenderlySimulation/TenderlySimulationButton.tsx
@@ -15,6 +15,53 @@ import {
 import { useEffect } from "react";
 import { useAccount } from "wagmi";
 
+const SAFE_ERROR_CODE: { [code: string]: string } = {
+  // General init related
+  GS000: "Could not finish initialization",
+  GS001: "Threshold needs to be defined",
+  GS002:
+    "A call to set up modules couldn't be executed because the destination account was not a contract",
+  //General gas/ execution related
+  GS010: "Not enough gas to execute Safe transaction",
+  GS011: "Could not pay gas costs with ether",
+  GS012: "Could not pay gas costs with token",
+  GS013: "Safe transaction failed when gasPrice and safeTxGas were 0",
+  //General signature validation related
+  GS020: "Signatures data too short",
+  GS021: "Invalid contract signature location: inside static part",
+  GS022: "Invalid contract signature location: length not present",
+  GS023: "Invalid contract signature location: data not complete",
+  GS024: "Invalid contract signature provided",
+  GS025: "Hash has not been approved",
+  GS026: "Invalid owner provided",
+  // General auth related
+  GS030: "Only owners can approve a hash",
+  GS031: "Method can only be called from this contract",
+  // Module management related
+  GS100: "Modules have already been initialized",
+  GS101: "Invalid module address provided",
+  GS102: "Module has already been added",
+  GS103: "Invalid prevModule, module pair provided",
+  GS104: "Method can only be called from an enabled module",
+  GS105: "Invalid starting point for fetching paginated modules",
+  GS106: "Invalid page size for fetching paginated modules",
+
+  // Owner management related
+  GS200: "Owners have already been set up",
+  GS201: "Threshold cannot exceed owner count",
+  GS202: "Threshold needs to be greater than 0",
+  GS203: "Invalid owner address provided",
+  GS204: "Address is already an owner",
+  GS205: "Invalid prevOwner, owner pair provided",
+
+  // Guard management related
+  GS300: "Transaction Guard does not implement IERC165",
+  GS301: "Module Guard does not implement IERC165",
+
+  // Fallback handler related
+  GS400: "Fallback handler cannot be set to self",
+};
+
 export default function TenderlySimulationButton({
   simulationArgs,
   shouldSimulate,
@@ -52,6 +99,18 @@ export default function TenderlySimulationButton({
     ) !== undefined;
   const isSuccess = data?.simulation?.status && !hasSafeExecutionFailureEvent;
   let errorMessage = error ? error.message : data?.simulation?.error_message;
+
+  if (!error && errorMessage) {
+    // try parse safe error code
+    const errCode = errorMessage as string;
+    const parse = SAFE_ERROR_CODE[errCode]
+      ? `${errCode} - ${SAFE_ERROR_CODE[errCode]}`
+      : errorMessage;
+    console.debug("tenderly simulation failed", parse, {
+      args: simulationArgs,
+    });
+  }
+
   if (errorMessage === undefined) {
     if (hasSafeExecutionFailureEvent) {
       errorMessage = "ExecutionFailure";
@@ -106,7 +165,7 @@ export default function TenderlySimulationButton({
             </div>
           ) : (
             <div className="flex items-center">
-              <Tooltip content={errorMessage}>
+              <Tooltip content={errorMessage} placement="top">
                 <XCircleIcon
                   className="-ml-0.5 h-5 w-5 text-red-400"
                   aria-hidden="true"
