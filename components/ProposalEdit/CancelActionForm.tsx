7da475a33de55ea18b7694a3e6eef790119eeb7a
twodam <twodam@protonmail.com>
2024-09-20 08:31:28 +0800

    fix(CancelActionForm): add missing cycleStart and count field

--- components/ProposalEdit/CancelActionForm.tsx
+++ components/ProposalEdit/CancelActionForm.tsx
@@ -1,28 +1,42 @@
 import SelectForm from "../form/SelectForm";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { SpaceContext } from "@/context/SpaceContext";
 import { useActions } from "@/utils/hooks/NanceHooks";
 import StringForm from "../form/StringForm";
 import ActionLabel from "../ActionLabel/ActionLabel";
+import { getEarliestStartCycle } from "@/utils/functions/GovernanceCycle";
+import UIntForm from "../form/UIntForm";
+import { useFormContext } from "react-hook-form";
 
 export default function CancelActionForm({
   genFieldName,
 }: {
   genFieldName: (field: string) => any;
 }) {
+  const { setValue } = useFormContext();
   const spaceInfo = useContext(SpaceContext);
   const spaceName = spaceInfo?.name || "";
 
   const { data } = useActions({ space: spaceName }, !!spaceName);
+
+  const earliestStartCycle = getEarliestStartCycle(
+    spaceInfo?.currentCycle || 1,
+    spaceInfo?.currentEvent.title || "Unknown"
+  );
 
   const options =
     data?.data?.map((r) => {
       return {
         value: r.action.uuid,
         displayValue: `${JSON.stringify(r.action.payload)}`,
+        // FIXME this ActionLabel doesn't render properly in single line, it lacks spaces
         displayComponent: <ActionLabel action={r.action} space={spaceName} />,
       };
     }) || [];
+
+  useEffect(() => {
+    setValue(genFieldName("count"), 1);
+  }, [setValue]);
 
   return (
     <div className="grid grid-cols-4 gap-6">
@@ -31,7 +45,6 @@ export default function CancelActionForm({
           label="Action to cancel"
           fieldName={genFieldName("targetActionUuid")}
           options={options}
-          defaultValue={options[0]?.value || "-- select --"}
           showType={false}
           tooltip="Which action do you want to cancel?"
         />
@@ -43,6 +56,21 @@ export default function CancelActionForm({
           fieldName={genFieldName("targetActionDescription")}
           showType={false}
         />
+      </div>
+
+      <div className="col-span-4 sm:col-span-1">
+        <UIntForm
+          label="Governance Cycle Start"
+          fieldName={genFieldName("cycleStart")}
+          defaultValue={earliestStartCycle}
+          min={earliestStartCycle}
+          showType={false}
+          tooltip="When should this action start to take effect?"
+        />
+        <span className="text-xs text-gray-400">
+          Current: GC-{spaceInfo?.currentCycle} (
+          {spaceInfo?.currentEvent.title || "Unknown"})
+        </span>
       </div>
     </div>
   );
