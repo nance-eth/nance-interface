6dabf1743307ac32472994e4e8298210f4a0f705
twodam <twodam@protonmail.com>
2024-10-10 10:13:05 +0800

    refactor(ContractHooks): comment out outdated function

--- utils/hooks/ContractHooks.ts
+++ utils/hooks/ContractHooks.ts
@@ -23,39 +23,39 @@ function transactionsToTuples(
   return [targets, values, calldatas];
 }
 
-export default function useQueueTransactionToContract(
-  address: string,
-  transactions: TransactionData[],
-  nonce?: string
-) {
-  // TODO to decide whether address is a safe or a governor
-  const { value, loading, error, trigger } = useQueueTransaction(
-    address,
-    transactions,
-    parseInt(nonce || "0")
-  );
-  const tuples = transactionsToTuples(transactions);
-  const {
-    data,
-    isPending: isLoading,
-    isSuccess,
-    writeContract,
-    request,
-  } = usePropose(
-    address as `0x${string}`,
-    tuples[0] as `0x${string}`[],
-    tuples[1],
-    tuples[2],
-    "Haha"
-  );
+// export default function useQueueTransactionToContract(
+//   address: string,
+//   transactions: TransactionData[],
+//   nonce?: string
+// ) {
+//   // TODO to decide whether address is a safe or a governor
+//   const { value, loading, error, trigger } = useQueueTransaction(
+//     address,
+//     transactions,
+//     parseInt(nonce || "0")
+//   );
+//   const tuples = transactionsToTuples(transactions);
+//   const {
+//     data,
+//     isPending: isLoading,
+//     isSuccess,
+//     writeContract,
+//     request,
+//   } = usePropose(
+//     address as `0x${string}`,
+//     tuples[0] as `0x${string}`[],
+//     tuples[1],
+//     tuples[2],
+//     "Haha"
+//   );
 
-  return {
-    value: value || data,
-    loading: loading || isLoading,
-    error: error || (isSuccess ? undefined : "Error proposing transaction"),
-    trigger: trigger || (() => writeContract(request!)),
-  };
-}
+//   return {
+//     value: value || data,
+//     loading: loading || isLoading,
+//     error: error || (isSuccess ? undefined : "Error proposing transaction"),
+//     trigger: trigger || (() => writeContract(request!)),
+//   };
+// }
 
 export enum ContractType {
   Safe,
