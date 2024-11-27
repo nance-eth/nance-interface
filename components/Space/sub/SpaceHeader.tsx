282b8dcb935bfaf9f433f48e08a8d404c891c560
twodam <twodam@protonmail.com>
2024-11-27 16:40:01 +0800

    feat(SpaceHeader): use countdown component class for transition effect; make info labels smaller to avoid catching too much focus

--- components/Space/sub/SpaceHeader.tsx
+++ components/Space/sub/SpaceHeader.tsx
@@ -5,18 +5,18 @@ import { parseISO, format, differenceInSeconds } from "date-fns";
 import { SpaceContext } from "@/context/SpaceContext";
 
 export function calculateRemainingTime(endTime: string) {
-  let remainingTime = "- - - -";
-  let formattedEndTime = "- - - -";
+  let remainingTime = [0, 0, 0, 0];
+  let formattedEndTime = "";
   try {
     const endTimeDate = parseISO(endTime);
     formattedEndTime = format(endTimeDate, "EEE MMM dd yyyy h:mm a");
     const difference = differenceInSeconds(endTimeDate, new Date());
     if (difference <= 0) return { remainingTime, formattedEndTime };
     const days = Math.floor(difference / (3600 * 24));
-    const hours = String(Math.floor((difference % (3600 * 24)) / 3600)).padStart(2, "0");
-    const minutes = String(Math.floor((difference % 3600) / 60)).padStart(2, "0");
-    const seconds = String(difference % 60).padStart(2, "0");
-    remainingTime = `${days} ${hours} ${minutes} ${seconds}`;
+    const hours = Math.floor((difference % (3600 * 24)) / 3600);
+    const minutes = Math.floor((difference % 3600) / 60);
+    const seconds = difference % 60;
+    remainingTime = [days, hours, minutes, seconds];
   } catch (error) {
     console.error("Error calculating remaining time:", error);
   }
@@ -25,13 +25,15 @@ export function calculateRemainingTime(endTime: string) {
 
 export default function SpaceHeader() {
   const spaceInfo = useContext(SpaceContext);
-  const [remainingTime, setRemainingTime] = useState("- -- -- --");
-  const [formattedEndTime, setFormattedEndTime] = useState("- -- -- --");
+  const [remainingTime, setRemainingTime] = useState<number[]>([0, 0, 0, 0]);
+  const [formattedEndTime, setFormattedEndTime] = useState("");
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (spaceInfo && spaceInfo.currentEvent) {
-        const { remainingTime, formattedEndTime } = calculateRemainingTime(spaceInfo.currentEvent.end || "");
+        const { remainingTime, formattedEndTime } = calculateRemainingTime(
+          spaceInfo.currentEvent.end || ""
+        );
         setRemainingTime(remainingTime);
         setFormattedEndTime(formattedEndTime);
       }
@@ -44,13 +46,8 @@ export default function SpaceHeader() {
     return null;
   }
 
-  const {
-    displayName,
-    currentEvent,
-    currentCycle,
-    snapshotSpace,
-    avatarURL
-  } = spaceInfo;
+  const { displayName, currentEvent, currentCycle, snapshotSpace, avatarURL } =
+    spaceInfo;
 
   return (
     <div className="mb-6 hidden max-w-7xl rounded-md bg-white p-6 shadow md:flex md:flex-col md:space-x-5">
@@ -70,28 +67,45 @@ export default function SpaceHeader() {
             </p>
           </div>
         </div>
-        <div className="rounded-md border-2 border-blue-600 bg-indigo-100 py-2 px-3 w-min-fit">
+        <div className="rounded-md border-2 border-blue-600 bg-indigo-100 py-2 px-3 w-min-fit text-xs">
           <div className="flex flex-col">
             <div className="flex flex-row items-center">
-              <div className="text-xs text-gray-500">Governance Cycle</div>
-              <div className="ml-2 font-semibold">{currentCycle}</div>
+              <div className="text-gray-500">Governance Cycle</div>
+              <div className="ml-2">{currentCycle}</div>
             </div>
             <div className="flex flex-row items-center">
-              <div className="text-xs text-gray-500">Current Event</div>
-              <div className="ml-8 font-semibold text-sm">{currentEvent.title}</div>
+              <div className="text-gray-500">Current Event</div>
+              <div className="ml-8">{currentEvent.title}</div>
             </div>
             <div className="flex justify-start items-center">
-              <div className="text-xs text-gray-500">Time Remaining</div>
+              <div className="text-gray-500">Time Remaining</div>
               <Tooltip content={formattedEndTime}>
                 <div className="ml-5">
-                  {remainingTime.split(" ").map((time, index) => (
-                    <span key={index} className="font-mono">
-                      {time}
-                      <span className="text-xs font-normal text-gray-500">
-                        {index === 0 ? "d " : index === 1 ? "h " : index === 2 ? "m " : "s"}
-                      </span>
-                    </span>
-                  ))}
+                  <span className="countdown font-mono">
+                    <span
+                      style={
+                        { "--value": remainingTime[0] } as React.CSSProperties
+                      }
+                    ></span>
+                    :
+                    <span
+                      style={
+                        { "--value": remainingTime[1] } as React.CSSProperties
+                      }
+                    ></span>
+                    :
+                    <span
+                      style={
+                        { "--value": remainingTime[2] } as React.CSSProperties
+                      }
+                    ></span>
+                    :
+                    <span
+                      style={
+                        { "--value": remainingTime[3] } as React.CSSProperties
+                      }
+                    ></span>
+                  </span>
                 </div>
               </Tooltip>
             </div>
