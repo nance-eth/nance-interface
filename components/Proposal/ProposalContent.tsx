553786ae72584f486423823741f0705fa69f46be
twodam <twodam@protonmail.com>
2024-12-10 20:57:24 +0800

    fix(ProposalNavigator): hide this simple numeric ProposalNavigator cause it can leads to non-existed proposal

--- components/Proposal/ProposalContent.tsx
+++ components/Proposal/ProposalContent.tsx
@@ -1,18 +1,15 @@
 import { useContext } from "react";
 import MarkdownViewer from "@/components/Markdown/MarkdownViewer";
 import { ProposalContext } from "./context/ProposalContext";
-import ProposalNavigator from "./sub/ProposalNavigator";
 
 export default function ProposalContent() {
   const { commonProps } = useContext(ProposalContext);
-  const sourceSnapshot = commonProps.uuid === "snapshot"; // hack
+  //const sourceSnapshot = commonProps.uuid === "snapshot"; // hack
   const { body } = commonProps;
 
   return (
     <div className="">
       <MarkdownViewer body={body} />
-
-      {!sourceSnapshot && <ProposalNavigator />}
     </div>
   );
 }
