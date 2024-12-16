f0d72d94298afd6d960355004f555425107a82f4
jigglyjams <jigglyjams@protonmail.com>
2024-12-16 16:33:00 -0500

    fix(TopAuthorTable): skeleton load the entire card

--- components/Analysis/TopAuthorTable.tsx
+++ components/Analysis/TopAuthorTable.tsx
@@ -14,63 +14,67 @@ export function TopAuthorTable({
   topAuthors,
 }: {
   loading: boolean;
-  topAuthors: TopAuthors[];
+  topAuthors?: TopAuthors[];
 }) {
   return (
-    <div className="card bg-base-100 w-80 sm:w-96 grow">
+    <div className={classNames(
+      "w-80 sm:w-96 grow",
+      loading ? "skeleton" : "card bg-base-100"
+    )}>
       <div className="card-body">
         <h2 className="card-title">Top Authors</h2>
         <div className="absolute top-6 right-6 flex items-center">
           <PoweredByNance />
         </div>
       </div>
-      <figure className={classNames("h-80", loading && "skeleton")}>
-        <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Author</th>
-                <th>Count</th>
-                <th>Approval Rate</th>
-              </tr>
-            </thead>
-            <tbody>
-              {topAuthors.map((author, index) => (
-                <tr key={index}>
-                  <td>
-                    <div className="flex items-center gap-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle h-8 w-8">
-                          <Image
-                            src={`https://cdn.stamp.fyi/avatar/${author.author}`}
-                            alt=""
-                            width={500}
-                            height={500}
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <div className="font-bold">
-                          <FormattedAddress
-                            address={author.author}
-                            copyable={false}
-                            link
-                            style="mt-1"
-                            minified
-                          />
-                        </div>
-                        <div className="text-sm opacity-50"></div>
-                      </div>
-                    </div>
-                  </td>
-                  <td>{author.count}</td>
-                  <td>{author.approvalRate}</td>
+      <figure className="h-80">
+        {!loading && (
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Author</th>
+                  <th>Count</th>
+                  <th>Approval Rate</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {topAuthors?.map((author, index) => (
+                  <tr key={index}>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-8 w-8">
+                            <Image
+                              src={`https://cdn.stamp.fyi/avatar/${author.author}`}
+                              alt=""
+                              width={500}
+                              height={500}
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">
+                            <FormattedAddress
+                              address={author.author}
+                              copyable={false}
+                              link
+                              style="mt-1"
+                              minified
+                            />
+                          </div>
+                          <div className="text-sm opacity-50"></div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>{author.count}</td>
+                    <td>{author.approvalRate}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </figure>
     </div>
   );
--- components/Analysis/VoterTurnoutChart.tsx
+++ components/Analysis/VoterTurnoutChart.tsx
@@ -91,5 +91,5 @@ export function VoterTurnoutChart({
         </ResponsiveContainer>
       </figure>
     </div>
-  )
+  );
 }
