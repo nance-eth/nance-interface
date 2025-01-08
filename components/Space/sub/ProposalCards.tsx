931aff14c341141cc1a9fd7489eb3a96564dea4b
twodam <twodam@protonmail.com>
2025-01-08 10:26:22 +0800

    fix(ProposalCards): dont display loading state if we are only loading second or later pages

--- components/Space/sub/ProposalCards.tsx
+++ components/Space/sub/ProposalCards.tsx
@@ -84,64 +84,64 @@ function sortProposals(
   }
 
   switch (sortBy) {
-  case "status":
-    proposals.sort(
-      (a, b) => getValueOfStatus(b.status) - getValueOfStatus(a.status)
-    );
-    break;
-  case "approval":
-    const sumScores = (p: Proposal) => {
-      return (p?.voteResults?.scores ?? []).reduce(
-        (partialSum, a) => partialSum + a,
-        0
+    case "status":
+      proposals.sort(
+        (a, b) => getValueOfStatus(b.status) - getValueOfStatus(a.status)
       );
-    };
-    proposals.sort((a, b) => sumScores(b) - sumScores(a));
-    break;
-  case "participants":
-    proposals.sort(
-      (a, b) => (b.voteResults?.votes ?? 0) - (a.voteResults?.votes ?? 0)
-    );
-    break;
-  case "voted":
-    const votedWeightOf = (p: Proposal) => {
-      const voted = votedData?.[p.voteURL!] !== undefined;
-      const hasSnapshotVoting = snapshotProposalDict[p.voteURL!];
+      break;
+    case "approval":
+      const sumScores = (p: Proposal) => {
+        return (p?.voteResults?.scores ?? []).reduce(
+          (partialSum, a) => partialSum + a,
+          0
+        );
+      };
+      proposals.sort((a, b) => sumScores(b) - sumScores(a));
+      break;
+    case "participants":
+      proposals.sort(
+        (a, b) => (b.voteResults?.votes ?? 0) - (a.voteResults?.votes ?? 0)
+      );
+      break;
+    case "voted":
+      const votedWeightOf = (p: Proposal) => {
+        const voted = votedData?.[p.voteURL!] !== undefined;
+        const hasSnapshotVoting = snapshotProposalDict[p.voteURL!];
 
-      if (hasSnapshotVoting) {
-        if (voted) return 2;
-        else return 1;
-      } else {
+        if (hasSnapshotVoting) {
+          if (voted) return 2;
+          else return 1;
+        } else {
+          return 0;
+        }
+      };
+      proposals.sort((a, b) => votedWeightOf(b) - votedWeightOf(a));
+      break;
+    case "title":
+      proposals.sort((a, b) => {
+        const nameA = a.title;
+        const nameB = b.title;
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+
+        // names must be equal
         return 0;
-      }
-    };
-    proposals.sort((a, b) => votedWeightOf(b) - votedWeightOf(a));
-    break;
-  case "title":
-    proposals.sort((a, b) => {
-      const nameA = a.title;
-      const nameB = b.title;
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-
-      // names must be equal
-      return 0;
-    });
-    break;
-  case "date":
-    proposals.sort((a, b) => {
-      const bD = b.createdTime || "";
-      const aD = a.createdTime || "";
-      return bD.localeCompare(aD);
-    });
-    break;
-  default:
-    proposals.sort();
-    break;
+      });
+      break;
+    case "date":
+      proposals.sort((a, b) => {
+        const bD = b.createdTime || "";
+        const aD = a.createdTime || "";
+        return bD.localeCompare(aD);
+      });
+      break;
+    default:
+      proposals.sort();
+      break;
   }
 
   if (!sortDesc) {
@@ -241,7 +241,9 @@ export default function ProposalCards({
     !skipSnapshotProposalsQuery &&
     data?.proposalsData === undefined;
   const isLoading =
-    proposalsLoading || snapshotLoading || willLoadSnapshotProposalsQuery;
+    // if we have proposalDataArray, we are only loading more pages, no need to display loading state
+    proposalDataArray === undefined &&
+    (proposalsLoading || snapshotLoading || willLoadSnapshotProposalsQuery);
 
   // convert proposalsData to dict with proposal id as key
   const snapshotProposalDict: { [id: string]: SnapshotProposal } = {};
