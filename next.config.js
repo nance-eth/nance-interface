d2592f41cbd6c0c04dbc24f080537306c45f61f3
jigglyjams <jigglyjams@protonmail.com>
2025-09-07 13:49:17 -0400

    fix: true

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: 'true',
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
